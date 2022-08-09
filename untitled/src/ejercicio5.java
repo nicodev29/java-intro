aab5c06e865c3b36dba2d51682de702f55418201
Nicolas Garcia <nicodev1229@gmail.com>
2022-08-08 23:24:10 -0300

    8-8-22
    hasta el 5

--- untitled/src/ejercicio5.java
+++ untitled/src/ejercicio5.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ejercicio5 {
     public static void main(String[] args) {
+
+        int num;
+
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero: ");
+        num = leer.nextInt();
+
+        System.out.println("El doble y el triple del num ingresado es: " + (num*2) + " y " + (num*3));
+        System.out.println("La raiz cuadrada es: " + Math.sqrt(num));
 
 
 
